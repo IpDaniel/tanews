58ba336c6e7cf8e3fc5f27c95d7b52719bc6781b
Naman Rusia <naman.rusia14@gmail.com>
2025-02-20 16:37:18 -0500

    topnav minor update

--- frontend/src/components/TopNav.jsx
+++ frontend/src/components/TopNav.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import "../styles/TopNav.css"; // stylesheet. go here to edit.
 
 const TopNav = () => {
-  return <div>TopNav</div>;
+  return (
+    <div className="topnav-container">
+      this is the topnav with simple styling.
+    </div>
+  );
 };
 
 export default TopNav;
