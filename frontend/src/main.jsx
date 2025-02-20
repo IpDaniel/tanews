8526471869cd7ccfd1a1ea36d281855dca3547a2
Naman Rusia <naman.rusia14@gmail.com>
2025-02-20 16:28:36 -0500

    set up react-router for project, look at Main.jsx for routes, and define others

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
 
-createRoot(document.getElementById('root')).render(
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+
+import App from "./routes/App.jsx";
+import Home from "./routes/Home.jsx";
+
+const router = createBrowserRouter([
+  { path: "/", element: <App /> },
+  { path: "/home", element: <Home /> },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router} />
+  </StrictMode>
+);
